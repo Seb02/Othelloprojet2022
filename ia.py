552bd7f1269f3206a31986e0c35e06922e26a35f
Sebastien Bokken <sebastien.bokken@gmail.com>
2022-05-01 16:27:55 +0200

    fin debug + insertion code venant du serveur + random fonctionnel

--- ia.py
+++ ia.py
@@ -2,7 +2,6 @@ import socket
 import sys
 import json
 import socket
-import game
 import random
 
 class othelloIA: #initialsation du socket 
@@ -32,10 +31,10 @@ class othelloIA: #initialsation du socket
 	def inscription(self):
 
 		renseignements = json.dumps({
-   		"request": "subscribe",
-   		"port": self.port,
-   		"name": self.name,
-   		"matricules": [self.matricule1, self.matricule2]
+		   "request": "subscribe",
+		   "port": self.port,
+		   "name": self.name,
+		   "matricules": [self.matricule1, self.matricule2]
 		}) #json.dumps() function converts a Python object into a json string.
 		self.s.send(renseignements.encode('utf8'))
 		self.PingPong()
@@ -63,7 +62,7 @@ class othelloIA: #initialsation du socket
 					print(messageread)
 					
 					reception = False
-			reception = True
+			print("test2")
 			if messageread == 	{"request": "ping"}:
 				pong = {"response": "pong"}
 				pongencode = json.dumps(pong)
@@ -77,16 +76,16 @@ class othelloIA: #initialsation du socket
 				jeublanc = messageread['state']["board"][1]
 				self.Etatjeu(self.color, jeunoir, jeublanc)
 				
-				self.mouvementspossibles = self.Mouvementspossibles(messageread['state']["board"])
+				self.PossibleMoves(messageread['state'])
 
 				if len(self.mouvementspossibles) > 0:
 					mouvement = self.Coupchoisi()
 					reponse = {"response": "move", "move": mouvement, "message": "Fun message"}
-					mouvementaenvoyer = json.dumps(pong)
+					mouvementaenvoyer = json.dumps(reponse)
 					client.send(mouvementaenvoyer.encode('utf8'))
 				else :
-					response = {"response": "giveup",}
-					mouvementaenvoyer = json.dumps(pong)
+					reponse = {"response": "giveup",}
+					mouvementaenvoyer = json.dumps(reponse)
 					client.send(mouvementaenvoyer.encode('utf8')) 
 
 		client.close()
@@ -111,17 +110,98 @@ class othelloIA: #initialsation du socket
 		else:
 			self.color = "white"
 
-	def Mouvementspossibles(self, board):
-		
-		game.possibleMoves()
+	
 	
 	def Coupchoisi(self):
 
 		return random.choice(self.mouvementspossibles)
+
+	def WillBeTaken(self, state, move):
+		playerIndex = state['current']
+		otherIndex = (playerIndex+1)%2
+
+		if not (0 <= move < 64):
+			raise BadMove('Your must be between 0 inclusive and 64 exclusive')
+
+		if move in state['board'][0] + state['board'][1]:
+			raise BadMove('This case is not free')
+
+		board = []
+		for i in range(2):
+			board.append(set((self.coord(index) for index in state['board'][i])))
+
+		move = self.coord(move)
+		
+
+		cases = set()
+
+		directions = [
+		( 0,  1),
+		( 0, -1),
+		( 1,  0),
+		(-1,  0),
+		( 1,  1),
+		(-1,  1),
+		( 1, -1),
+		(-1, -1)
+		]
+		for direction in directions:
+			mayBe = set()
+			for case in self.walk(move, direction):
+				if case in board[otherIndex]:
+					mayBe.add(case)
+				elif case in board[playerIndex]:
+					cases |= mayBe
+					break
+				else:
+					break
+
+		if len(cases) == 0:
+			raise BadMove('Your move must take opponent\'s pieces')
+	
+		return [self.index(case) for case in cases]
+
+	def index(self, coord):
+		l, c = coord
+		return l*8+c
+
+	def isInside(self, coord):
+		l, c = coord
+		return 0 <= l < 8 and 0 <= c < 8	
+
+	def walk(self, start, direction):
+		current = start
+		while self.isInside(current):
+			current = self.add(current, direction)
+			yield current
+
+
+	def PossibleMoves(self, state):
+		res = []
+		for move in range(64):
+			try:
+				self.WillBeTaken(state, move)
+				res.append(move)
+			except BadMove:
+				pass
+		self.mouvementspossibles = res
+
+
+
+	def coord(self, index):
+		return index // 8, index % 8
+
+
+	def add(self, p1, p2):
+		l1, c1 = p1
+		l2, c2 = p2
+		return l1 + l2, c1 + c2
+		
 		
 
 	
-	
+class BadMove(Exception): #pour définir une erreur d'un type défini
+	pass
 
 
 
